1af9dbd575faccf29731ad52f3fb600a62661433
Xiaoji Chen <cxiaoji@gmail.com>
2024-08-27 00:03:22 -0700

    Use shared module for website

--- website/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export {default as ExamplesIndex} from './example/examples-index';
-
-export {default as Home} from './home';
-export {default as InfoPanel} from './info-panel';
--- website/src/utils/data-utils.js
+++
@@ -1,7 +0,0 @@
-export function joinPath(base, path) {
-  if (path.match(/^\w+:\/\//)) {
-    // has protocol
-    return path;
-  }
-  return `${base.replace(/\/$/, '')}/${path.replace(/^\//, '')}`;
-}
