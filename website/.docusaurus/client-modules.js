7c832f01dd2d2dcef37b4c3ec865b40124ea2555
Xiaoji Chen <cxiaoji@gmail.com>
2024-08-26 19:31:32 -0700

    Add docusaurus artifacts to gitignore

--- website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/missx/Source/mjolnir.js/website/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/missx/Source/mjolnir.js/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/missx/Source/mjolnir.js/website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/missx/Source/mjolnir.js/website/src/styles.css'),
-];
